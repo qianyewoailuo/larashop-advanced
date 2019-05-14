3707e5d3287c1c57dc3535db56fb45c59f95566b
qianyewoailuo <qianyewoailuo@126.com>
2019-05-14 03:47:35 +0000

    heroku pgsql

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -4,13 +4,29 @@
  * @Author: luo
  * @email: qianyewoailuo@126.com
  * @Date: 2019-05-14 09:49:18
- * @LastEditTime: 2019-05-14 10:35:27
+ * @LastEditTime: 2019-05-14 11:40:20
  */
 // 测试辅助函数是否正常引入
 function test_helper()
 {
     return 'ok';
 }
+// 根据不同运行环境指定数据库配置信息
+function get_db_config()
+{
+    if (getenv('IS_IN_HEROKU')) {
+        $url = parse_url(getenv("DATABASE_URL"));
+
+        return $db_config = [
+            'connection' => 'pgsql',
+            'host'  => $url['host'],
+            'database' => substr($url['path'], 1),
+            'username' => $url['user'],
+            'password' => $url['pass'],
+        ];
+    }
+}
+
 // 当前请求的路由名称转换为 CSS 类名称
 function route_class()
 {
--- config/database.php
+++ config/database.php
@@ -1,5 +1,6 @@
 <?php
-
+// 环境配置数据库参数
+$db_config = get_db_config();
 return [
 
     /*
@@ -13,7 +14,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    // 'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => getenv('IS_IN_HEROKU') ? 'pgsql' : env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,13 +58,14 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $db_config['host'],
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => $db_config['database'],
+            'username' => $db_config['username'],
+            'password' => $db_config['password'],
             'charset' => 'utf8',
             'prefix' => '',
+            'prefix_indexes' => true,
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
