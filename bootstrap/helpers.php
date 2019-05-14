57069f954f647ea0ccb4c2e6f59ed532d8d84fb2
qianyewoailuo <qianyewoailuo@126.com>
2019-05-14 03:09:12 +0000

    页面基本布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -4,10 +4,15 @@
  * @Author: luo
  * @email: qianyewoailuo@126.com
  * @Date: 2019-05-14 09:49:18
- * @LastEditTime: 2019-05-14 09:53:13
+ * @LastEditTime: 2019-05-14 10:35:27
  */
 // 测试辅助函数是否正常引入
 function test_helper()
 {
     return 'ok';
+}
+// 当前请求的路由名称转换为 CSS 类名称
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
 }
