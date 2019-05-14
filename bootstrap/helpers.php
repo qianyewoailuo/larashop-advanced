799186598bd4e7e91fc62e8d8f7b747c36ab4295
qianyewoailuo <qianyewoailuo@126.com>
2019-05-14 01:55:43 +0000

    helper function

---
+++ bootstrap/helpers.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * @Description: 辅助函数 in larashop
+ * @Author: luo
+ * @email: qianyewoailuo@126.com
+ * @Date: 2019-05-14 09:49:18
+ * @LastEditTime: 2019-05-14 09:53:13
+ */
+// 测试辅助函数是否正常引入
+function test_helper()
+{
+    return 'ok';
+}
