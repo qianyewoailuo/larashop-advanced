d31dda25f756124a38969ee0939e9ee828623cf8
qianyewoailuo <qianyewoailuo@126.com>
2019-05-16 16:02:12 +0000

    商品加入购物车

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,10 @@ Route::group(['middleware'=>'auth'],function(){
         Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
         // favorites 收藏商品列表
         Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
+
+        // Cart 购物车
+        Route::post('cart','CartController@add')->name('cart.add');
+
     });
 
     // 结束
