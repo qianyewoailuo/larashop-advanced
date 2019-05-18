6fe51a36bc50761d0516de70176a9b87892999a7
qianyewoailuo <qianyewoailuo@126.com>
2019-05-18 18:33:57 +0000

    引入yansongda/pay支付库

---
+++ config/pay.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * 微信支付与支付宝支付接口配置信息
+ */
+
+return [
+    // 支付宝支付配置信息
+    'alipay' => [
+        'app_id'         => '',
+        'ali_public_key' => '',
+        'private_key'    => '',
+        'log'            => [
+            'file' => storage_path('logs/alipay.log'),
+        ],
+    ],
+    // 微信支付配置信息
+    'wechat' => [
+        'app_id'      => '',
+        'mch_id'      => '',
+        'key'         => '',
+        'cert_client' => '',
+        'cert_key'    => '',
+        'log'         => [
+            'file' => storage_path('logs/wechat_pay.log'),
+        ],
+    ],
+];
