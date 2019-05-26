214c73204cf3a3edb2f087d3c842a09d45c26a0a
qianyewoailuo <qianyewoailuo@126.com>
2019-05-26 14:01:29 +0000

    初始化众筹表与模型

---
+++ app/Models/CrowdfundingProduct.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CrowdfundingProduct extends Model
+{
+
+    // 定义众筹的 3 种状态
+    const STATUS_FUNDING = 'funding';
+    const STATUS_SUCCESS = 'success';
+    const STATUS_FAIL    = 'fail';
+
+    public static $statusMap = [
+        self::STATUS_FUNDING => '众筹中',
+        self::STATUS_SUCCESS => '众筹成功',
+        self::STATUS_FAIL    => '众筹失败',
+    ];
+
+    protected $fillable = [
+        'total_amount','target_amount','user_amount','status','end_at'
+    ];
+
+    protected $dates = ['end_at'];
+
+    public $timestamps = false;
+
+    // 一个众筹属于某个商品
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    // 定义一个名为 percent 的访问器，返回当前众筹百分比进度
+    public function getPercentAttribute()
+    {
+        // 已筹金额除以目标金额
+        $value = $this->attributes['total_amount'] / $this->attributes['target_amount'];
+
+        // floatval 函数用于获取变量的浮点值
+        // number_format 函数通过千位分组来格式化数字 例如
+        // echo number_format('543210', 2, '.', '');
+        // 输出为 543210.00
+        return floatval(number_format($value * 100,2,'.',''));
+    }
+}
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,9 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 class Product extends Model
 {
+
+    const TYPE_NORMAL       = 'normal';
+    const TYPE_CROWDFUNDING = 'crowdfunding';
+
+    public static $typeMap = [
+        self::TYPE_NORMAL       => '普通商品',
+        self::TYPE_CROWDFUNDING => '众筹商品',
+    ];
+
     protected $fillable = [
         'title', 'description', 'image', 'on_sale',
-        'rating', 'sold_count', 'review_count', 'price'
+        'rating', 'sold_count', 'review_count', 'price',
+        'type',
     ];
     protected $casts = [
         'on_sale' => 'boolean', // on_sale 是一个布尔类型的字段
@@ -22,6 +32,11 @@ class Product extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+    // 一个商品有一个众筹
+    public function crowdfunding()
+    {
+        return $this->hasOne(CrowdfundingProduct::class);
     }
 
     // 获取完整imgageUrl属性
